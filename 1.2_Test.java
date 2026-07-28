5d3f29da21411120255a0c28c18d6243d8c46465
Luongdt116 <tranluongicd16@gmail.com>
2026-07-28 18:32:34 +0700

    Delete 1.2_Test.java

--- 1.2_Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        Circle circle = new Circle();
-
-        circle.radius = 12;
-
-        System.out.println(String.format("Area of the circle %2f", circle.getArea()));
-    }
-}
