6071454afda279bf3430c4f77a4aad379d37fb77
Luongdt116 <tranluongicd16@gmail.com>
2026-08-04 08:45:12 +0700

    commit lab04

---
+++ 4.2_Clothing.java
@@ -0,0 +1,24 @@
+package Lab04;
+
+public class Clothing extends Product{
+    private String size;
+
+    public Clothing(int id, String name, float price, String size) {
+        super(id,name,price);
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Vase [ID: %-5s | name: %-6s | price: %-6d | size: % -3s ]",
+                id, name, price, size);
+    }
+}
