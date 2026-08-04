de0edbcbe752d62418c8e88122946a85572f4090
Luongdt116 <tranluongicd16@gmail.com>
2026-08-04 08:44:39 +0700

    commit lab04

---
+++ 4.1_Product.java
@@ -0,0 +1,43 @@
+package Lab04;
+
+public abstract class Product {
+    protected  int id;
+    protected String name;
+    protected float price;
+
+    public Product(int id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Vase [ID: %-5s | name: %-6s | price: %-6d ]",
+                id, name, price);
+    }
+}
